4103fc5407479a347a072bb56bc3d64af357a41f
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-06 16:44:24 +0100

    Added interfacetest.js

---
+++ interfacetest.js
@@ -0,0 +1,24 @@
+var readline = require('readline');
+var util = require('util');
+var RL = readline.createInterface(proces.stdin, process.stout);
+
+RL.question('What is your name? ', (name)=>{
+  //Set up the prompt
+  RL.setPrompt(`${name} how old are you?` );
+  //Set up the prompt in ther terminal
+  RL.prompt();
+  //Set up the listener
+  RL.on('line', (age)=>{
+
+    if (age<18){
+      util.log(`${name.trim()} because you are ${age} years old, you can't enter.`);
+      RL.close();
+    }else{
+      util.log(`${name.trim()} you are ${age} years old, you can enter.`);
+      RL.close();
+    }
+  });
+});
+
+//Now go to the terminal, type 'node', then 'node interfacetest.js'
+//You should receive a prompt to enter you name and age.
