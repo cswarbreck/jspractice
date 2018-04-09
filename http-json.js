c3bd55ac2eec4dba44aec1c23e82b0685941f847
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-10 00:35:50 +0100

    Added a boilerplate http submit-form and accompanying html file

---
+++ http-json.js
@@ -0,0 +1,50 @@
+const https = require('https');
+const http = require ('http');
+const fs = require ('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+http.createServer((req, serverRes)=>{
+
+    if (req.method === 'GET' && req.url === './posts'){
+
+        https.get(url, (httpRes)=>{
+
+            httpRes.on('data', data => {
+
+                httpRes.setEncoding('utf8');
+
+                console.log(data);
+
+                serverRes.write(data);
+            });
+
+            httpRes.on('end', ()=>{
+
+                serverRes.end();
+
+                console.log('...aaaaaaand DONE!');
+            });
+        
+        
+        
+        });
+
+    }else{
+
+        serverRes.writeHead(404, {'Content-Type': 'text/plain'});
+
+        serverRes.end('404 ERROR');
+
+
+
+
+    }
+
+
+}).listen(4444);
+
+console.log('The void stares back....');
+
+//Need to go to localhost:4444/posts
+
